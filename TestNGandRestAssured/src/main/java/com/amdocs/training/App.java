6aa8d49ef4cef913ee9cca34af3970e367880935
RUCHIK <Ruchi.Kachroo@amdocs.com>
2020-08-16 13:41:48 +0530

    this is commit from remote machine

--- TestNGandRestAssured/src/main/java/com/amdocs/training/App.java
+++ TestNGandRestAssured/src/main/java/com/amdocs/training/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( " WElcome to GitHub!" );
+        System.out.println( "Well done!" );
     }
 }
